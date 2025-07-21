76111c10c918c4a67e798fdf3afafd2e660273a6
lukmanHl <tec.lukman@gmail.com>
2025-07-21 21:55:08 +0100

    i just made some changes

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # lukmanPortfolio
+# this is a copy of my portfolio and this is done with the basics of html, css and js 
--- script.js
+++ script.js
@@ -13,3 +13,22 @@ if (close) {
         nav.classList.remove('active');
     })
 }
+
+
+
+
+// for carousel
+
+let currentIndex = 0;
+
+function moveSlide(direction) {
+  const items = document.querySelectorAll('.carousel-item');
+  const totalItems = items.length;
+
+  items[currentIndex].classList.remove('active');
+  currentIndex = (currentIndex + direction + totalItems) % totalItems;
+  items[currentIndex].classList.add('active');
+
+  const inner = document.querySelector('.carousel-inner');
+  inner.style.transform = `translateX(-${currentIndex * 100}%)`;
+}
